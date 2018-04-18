6314c97e6387074bc69e17d2f10e48e3aded2373
erwehe <erikhedman2@gmail.com>
2018-04-18 18:38:58 +0200

    added folders and created a file with code for executing canu from terminal line

---
+++ code/02_terminal_canu.sh
@@ -0,0 +1,5 @@
+./canu -assemble \
+-p 01_ls_assembly_result -d home/erwehe/Desktop/genome_analysis/github/ga18/analyses/02_genome_assembly \
+genomeSize=2.61m \
+stopOnReadQuality=false \
+-pacbio-raw home/erwehe/Desktop/genome_analysis/DNA_raw_data/ERR2028495.fastq.gz
