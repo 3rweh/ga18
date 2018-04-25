e61ce0b00a7e12005a064ed570a906d0a21ed0bf
erwehe <erikhedman2@gmail.com>
2018-04-25 17:12:18 +0200

    started the bash file for the BWA

---
+++ code/05_bwa_bash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 08:30:00
+#SBATCH -J assembly_qualitycheck
+#SBATCH --mail-typ=ALL
+#SBATCH --mail-user erikhedman2@gmail.com
+
+# Load modules
+module load bioinfo-tools bwa
+
+# Your commands
+
