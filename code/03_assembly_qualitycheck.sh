2c1ed756e9703249dd6ba926536355e65458fe02
erwehe <erikhedman2@gmail.com>
2018-04-25 14:38:38 +0200

    Started the file for the genome assembly check

---
+++ code/03_assembly_qualitycheck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 00:30:00
+#SBATCH -J assembly_qualitycheck
+#SBATCH --mail-typ=ALL
+#SBATCH --mail-user erikhedman2@gmail.com
+
+# Load modules
+module load bioinfo-tools python quast
+
+# Your commands
+./quast -o /home/erwehe/github/ga18/analyses/
+/home/erwehe/github/ga18/analyses/02_genome_assembly/leptospirillum_assembly_result.contigs.fasta
+-R /proj/g2018003/nobackup/private/christel_2017/reference/OBMB01.fasta
