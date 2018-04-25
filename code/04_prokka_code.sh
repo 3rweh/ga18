b4407ba3fce724848dd0966dba82dea8b2b0c3d1
erwehe <erikhedman2@gmail.com>
2018-04-25 16:09:39 +0200

    code for the prokka step

---
+++ code/04_prokka_code.sh
@@ -0,0 +1,1 @@
+prokka /home/erwehe/github/ga18/analyses/02_genome_assembly/leptospirillum_assembly_result.contigs.fasta --outdir /home/erwehe/github/ga18/analyses/03_genome_annotation --prefix leptospir
