fb7ef09da315d02a4e7c47b7bfb5f3e121915a43
erwehe <erikhedman@gmail.com>
2018-04-26 12:35:10 +0200

    code for trimmomatic ready to be tested

--- code/06_trimmometric_bash.sh
+++ code/06_trimmometric_bash.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -t 02:00:00
-#SBATCH -J trimmomatic_RNAraw
+#SBATCH -J trimmomatic_RNA_raw_data
 #SBATCH --mail-typ=ALL
 #SBATCH --mail-user erikhedman2@gmail.com
 
@@ -12,5 +12,21 @@
 module load bioinfo-tools 
 
 # Your commands
-trimmomatic
-/proj/g2018003/nobackup/private/christel_2017/RNA_raw_data/ERR*.fastq.gz
+java -jar trimmomatic-0.35.jar PE -threads 1 \
+# infiles
+/proj/g2018003/nobackup/private/christel_2017/RNA_raw_data/ERR2036629_1.fastq.gz \
+/proj/g2018003/nobackup/private/christel_2017/RNA_raw_data/ERR2036629_2.fastq.gz \
+# outfiles
+/home/erwehe/github/ga18/analyses/01_preprocessing/ERR2036629_P1.trim.fastq.gz \
+/home/erwehe/github/ga18/analyses/01_preprocessing/ERR2036629_P2.trim.fastq.gz \
+/home/erwehe/github/ga18/analyses/01_preprocessing/ERR2036629_S1.trim.fastq.gz \
+/home/erwehe/github/ga18/analyses/01_preprocessing/ERR2036629_S2.trim.fastq.gz \
+# :seed_mismatch:palindrome_clip:simple_clip
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 \
+LEADING:20 \
+TRAILING:20 \
+# :window_size:window_quality
+SLIDINGWINDOW:1:3 \
+MINLEN:40 \
+# :target_length:strictness
+MAXINFO:40:0.5 
