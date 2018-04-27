cbd6f550c2b620be86fe8273612649873affbb58
erwehe <erikhedman2@gmail.com>
2018-04-27 17:05:14 +0200

    Added an integrated converter so that the out files don't end the storage. Got som good help from Ghuile. Hopefully it works.

--- code/05_bwa_bash.sh
+++ code/05_bwa_bash.sh
@@ -13,34 +13,34 @@ module load bioinfo-tools
 module load bwa/0.7.17
 
 # Your commands
-bwa index -p index_BWA /home/erwehe/github/ga18/analyses/02_genome_assembly/leptospirillum_assembly_result.contigs.fasta \
+bwa index -p index_BWA /home/erwehe/github/ga18/analyses/02_genome_assembly/leptospirillum_assembly_result.contigs.fasta;\
 bwa mem index_BWA \
 /home/erwehe/github/ga18/analyses/01_preprocessing/ERR2036629_P1.trim.fastq.gz \
-/home/erwehe/github/ga18/analyses/01_preprocessing/ERR2036629_P2_trim.fastq.gz > 29_bwa_mem.sam;\
+/home/erwehe/github/ga18/analyses/01_preprocessing/ERR2036629_P2_trim.fastq.gz | samtools view -bShu - | samtools sort - 29_bwa_mem.bam.sorted;\
 bwa mem index_BWA \
 /proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036630_P1.trim.fastq.gz \
-/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036630_P2.trim.fastq.gz > 30_bwa_mem.sam;\
+/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036630_P2.trim.fastq.gz | samtools view -bShu - | samtools sort - 30_bwa_mem.bam.sorted;\
 bwa mem index_BWA \
 /proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036631_P1.trim.fastq.gz \
-/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036631_P2.trim.fastq.gz > 31_bwa_mem.sam;\
+/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036631_P2.trim.fastq.gz | samtools view -bShu - | samtools sort - 31_bwa_mem.bam.sorted;\
 bwa mem index_BWA \
 /proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036632_P1.trim.fastq.gz \
-/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036632_P2.trim.fastq.gz > 32_bwa_mem.sam;\
+/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036632_P2.trim.fastq.gz | samtools view -bShu - | samtools sort - 32_bwa_mem.bam.sorted;\
 bwa mem index_BWA \
 /proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036633_P1.trim.fastq.gz \
-/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036633_P2.trim.fastq.gz > 33_bwa_mem.sam;\
+/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2036633_P2.trim.fastq.gz | samtools view -bShu - | samtools sort - 33_bwa_mem.bam.sorted;\
 bwa mem index_BWA \
 /proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117288_P1.trim.fastq.gz \
-/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117288_P2.trim.fastq.gz > 88_bwa_mem.sam;\
+/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117288_P2.trim.fastq.gz | samtools view -bShu - | samtools sort - 88_bwa_mem.bam.sorted;\
 bwa mem index_BWA \
 /proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117289_P1.trim.fastq.gz \
-/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117289_P2.trim.fastq.gz > 89_bwa_mem.sam;\
+/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117289_P2.trim.fastq.gz | samtools view -bShu - | samtools sort - 89_bwa_mem.bam.sorted;\
 bwa mem index_BWA \
 /proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117290_P1.trim.fastq.gz \
-/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117290_P2.trim.fastq.gz > 90_bwa_mem.sam;\
+/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117290_P2.trim.fastq.gz | samtools view -bShu - | samtools sort - 90_bwa_mem.bam.sorted;\
 bwa mem index_BWA \
 /proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117291_P1.trim.fastq.gz \
-/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117291_P2.trim.fastq.gz > 91_bwa_mem.sam;\
+/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117291_P2.trim.fastq.gz | samtools view -bShu - | samtools sort - 91_bwa_mem.bam.sorted;\
 bwa mem index_BWA \
 /proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117292_P1.trim.fastq.gz \
-/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117292_P2.trim.fastq.gz > 92_bwa_mem.sam
+/proj/g2018003/nobackup/private/christel_2017/RNA_trimmed_reads/ERR2117292_P2.trim.fastq.gz | samtools view -bShu - | samtools sort - 92_bwa_mem.bam.sorted
