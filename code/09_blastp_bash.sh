7f8252d3f9f5713822e073e31f463940092fa95f
erwehe <erikhedman2@gmail.com>
2018-05-11 14:34:19 +0200

    no changes should not have been done.

--- code/09_blastp_bash.sh
+++ code/09_blastp_bash.sh
@@ -2,9 +2,9 @@
 
 #SBATCH -A g2018003
 #SBATCH -p core
-#SBATCH -n 1
-#SBATCH -t 02:00:00
-#SBATCH -J blastp__with_prokkaresult
+#SBATCH -n 2
+#SBATCH -t 10:00:00
+#SBATCH -J blastp_with_prokkaresult
 #SBATCH --mail-typ=ALL
 #SBATCH --mail-user erikhedman2@gmail.com
 
@@ -13,4 +13,4 @@ module load bioinfo-tools blast/2.6.0+
 
 # Your code
 blastp -db nr -query /home/erwehe/github/ga18/analyses/03_genome_annotation/prokka/leptospir.faa \
--out /home/erwehe/github/ga18/analyses/08_blastp_output/blastp_out -outfmt 6 -max_target_seqs 20
+-out /home/erwehe/github/ga18/analyses/08_blastp_output/blastp_out.txt -outfmt 6 -max_target_seqs 20
