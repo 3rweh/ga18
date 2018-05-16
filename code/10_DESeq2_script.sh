63a61a10268c87f10fb8baa7ceb1dcec08af28be
erwehe <erikhedman2@gmail.com>
2018-05-16 17:00:20 +0200

    Developed the script for the DESeq2 analysis. Not done.

--- code/10_DESeq2_script.sh
+++ code/10_DESeq2_script.sh
@@ -1,8 +1,14 @@
 directory <- "/home/erwehe/github/ga18/analyses/07_HTSeq_output"
-sampleFiles <- grep("count",list.files(directory),value=TRUE)
-sampleCondition <- sub("(.*count).*","\\1",sampleFiles)
+sampleFiles <- grep(".txt",list.files(directory),value=TRUE)
+sampleCondition <- sub("(.*cont).*","\\1",sampleFiles)
 sampleTable <- data.frame(sampleName=sampleFiles, fileName=sampleFiles, condition=sampleCondition)
 library("DESeq2")
 ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable=sampleTable, directory=directory, design=~condition)
 ddsHTSeq
+keep <- rowSums(counts(ddsHTSeq)) > 0
+ddsHTSeq <- ddsHTSeq[keep,]
+ddsHTSeq$condition <- factor(ddsHTSeq$condition, levels = c("cont","mineral"))
+ddsHTSeq <- DESeq(ddsHTSeq)
+res <- results(ddsHTSeq, contrast=c("condition","cont","mineral"))
+
 
